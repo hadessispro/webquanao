4c67de805eddde3fd99fb037f47dd3d8270f84ef
hadessispro <thaibaoleo123@gmail.com>
2026-08-10 12:49:46 +0700

    fix(ProductDetailClient): add missing useEffect import - fixes product page crash (ReferenceError: useEffect is not defined)

--- california-arts-nextjs/src/components/product/ProductDetailClient.tsx
+++ california-arts-nextjs/src/components/product/ProductDetailClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useLayout } from "@/context/LayoutContext";
